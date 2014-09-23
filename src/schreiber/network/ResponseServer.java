40b7d04d7db6b8d3b5af2151322d048e46d17007
aschreib <aschreib@student.touro.edu>
2014-09-23 18:34:49 -0400

    made ResponseServer multi-threaded
    made a server that extends thread class and put the code in the run
    method

--- src/schreiber/network/ResponseServer.java
+++ src/schreiber/network/ResponseServer.java
@@ -1,12 +1,7 @@
 package schreiber.network;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class ResponseServer {
 
@@ -14,26 +9,7 @@ public class ResponseServer {
 
 		ServerSocket serverSocket = new ServerSocket(8080);
 
-		int counter = 0;
-		while (true) {
-			Socket socket = serverSocket.accept();
-
-			InputStream in = socket.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-			String line;
-			while (!"".equals((line = reader.readLine()))) {
-				System.out.println(line);
-			}
-
-			OutputStream out = socket.getOutputStream();
-			String response = "<h2>This is the " + counter + "th request</h2>";
-			out.write("HTTP/1.1 200 OK\n".getBytes());
-			out.write("Content-Type: text/html; charset=utf-8\n".getBytes());
-			out.write(("Content-Length: "+response.length()+"\n\n").getBytes());
-			out.write(response.getBytes());
-			out.flush();
-			out.close();
-
-		}
+		ResponseServerThread thread = new ResponseServerThread(serverSocket);
+		thread.start();
 	}
 }
