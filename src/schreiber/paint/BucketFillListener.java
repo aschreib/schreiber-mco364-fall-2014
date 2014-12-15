5c751f224c0e8cdd1249fe93b06d017fceb1ada6
aschreib <aschreib@student.touro.edu>
2014-12-15 12:37:25 -0500

    working on bucketFill
    recursion algorithm

--- src/schreiber/paint/BucketFillListener.java
+++ src/schreiber/paint/BucketFillListener.java
@@ -3,6 +3,7 @@ package schreiber.paint;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 public class BucketFillListener implements DrawListener {
 
@@ -10,9 +11,40 @@ public class BucketFillListener implements DrawListener {
 
 	private int x, y;
 	private Color color;
+	private BufferedImage image;
+	private int colorClicked;
 
 	public BucketFillListener(Canvas canvas) {
 		this.canvas = canvas;
+		image = canvas.getBufferedImage();
+	}
+
+	public void Fill(int x, int y, Color color, int colorClicked) {
+
+		if (colorClicked == color.getRGB()) {
+			return;
+		}
+		if (color.getRGB() == image.getRGB(x, y)) {
+			return;
+		}
+		image.setRGB(x, y, colorClicked);
+		if (validCoordinate(x - 1, y)) {
+			Fill(x - 1, y, color, colorClicked);
+		}
+		if (validCoordinate(x + 1, y)) {
+			Fill(x + 1, y, color, colorClicked);
+		}
+		if (validCoordinate(x, y - 1)) {
+			Fill(x, y - 1, color, colorClicked);
+		}
+		if (validCoordinate(x, y + 1)) {
+			Fill(x, y + 1, color, colorClicked);
+		}
+		return;
+	}
+
+	public boolean validCoordinate(int x, int y) {
+		return (x >= 0 && x <= 800 && y >= 0 && y <= 600);
 	}
 
 	@Override
@@ -39,6 +71,12 @@ public class BucketFillListener implements DrawListener {
 		x = e.getX();
 		y = e.getY();
 		color = canvas.getColor();
+		System.out.println(color.getRGB());
+		colorClicked = image.getRGB(x, y);
+		System.out.println(colorClicked);
+		Fill(x, y, color, colorClicked);
+		canvas.repaint();
+		System.out.println("done");
 	}
 
 	@Override
